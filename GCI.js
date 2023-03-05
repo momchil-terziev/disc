5e7773af44e71213a56651fe9b3cb05e0c4a3fa9
momchil-terziev <97561226+momchil-terziev@users.noreply.github.com>
2023-03-05 13:39:48 +0000

    Update GCI.js

--- GCI.js
+++ GCI.js
@@ -1,26 +1,32 @@
 function GCI() {
-
   if (r21 == r32) {
     reftype.html("Constant refinement factor used");
-    qlabel.html(" ")
+    qlabel.html(" ");
+    plabel.html("Order of accuracy: " + p);
+    delta = e21 / (pow(r21, p) - 1);
+    U = ((1.25 * delta) / f) * 100;
+    deltalabel.html("Error: " + delta);
   } else {
     var i = 0;
-    iteratePandQ(0,p);
+    iteratePandQ(0, p);
     reftype.html("Variable refinement factor used " + " ");
-qlabel.html("q: " + q)
+    qlabel.html("q: " + q);
+    plabel.html("Order of accuracy: " + pcor);
+
+    delta = e21 / (pow(r21, pcor) - 1);
+    U = ((1.25 * delta) / f) * 100;
+
+    deltalabel.html("Error: " + delta);
   }
 
-  delta = e21 / (pow(r21, p) - 1);
   fext = f - delta;
-  U = ((1.25 * delta)/f) * 100;
 
-  plabel.html("Order of accuracy: " + p);
+  // plabel.html("Order of accuracy: " + p);
   Rlabel.html("Refinement ratio: " + R);
   e21label.html("e21: " + e21);
   e32label.html("e32: " + e32);
   fextlabel.html("Extrapolated solution: " + fext);
-  deltalabel.html("Error: " + delta);
   uncertaintylabel.html("Uncertainty: " + U + "%");
-  Plabel.html(" ")
-  CFlabel.html(" ")
+  Plabel.html(" ");
+  CFlabel.html(" ");
 }
